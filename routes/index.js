2d9b19fbe0349a273e20a76a6291cc5577f75c0f
lilys0 <lsouryaphanh@gmail.com>
2021-11-05 22:03:26 +1100

    delete note

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const notes = require("express").Router();
-const { readFromFile, readAndAppend } = require("../helpers/fs_utils");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fs_utils");
 const uuid = require("../helpers/uuid");
 
 // GET Route for retrieving all the notes
@@ -39,4 +43,20 @@ notes.post("/", (req, res) => {
   }
 });
 
+// DELETE Route for a specific note
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile("./db/db.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 module.exports = notes;
